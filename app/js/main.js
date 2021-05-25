f01eb9a5e1e7588b79bda37076d7b3d7ed11cab5
TetianaDro <tdrobovska@gmail.com>
2021-05-25 11:50:04 +0300

    fixed filter mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -5,10 +5,22 @@ $(function(){
     arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 2000 
+    autoplaySpeed: 2000
   });
 
 
-  var mixer = mixitup('.week-products__inner');
-  var mixer = mixitup('.new-design__inner');
+  // var mixer = mixitup('.week-products__inner');
+  // var mixer = mixitup('.new-design__inner');
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 });
